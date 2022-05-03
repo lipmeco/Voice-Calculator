3be953a1c68e6ffa1e9d68a391e013a2650c665a
Maxim <maksismart200@gmail.com>
2022-05-03 16:02:05 +0300

    Create project.

---
+++ Voice Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Voice Calculator
+//
+//  Created by Максим Кузнецов on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
